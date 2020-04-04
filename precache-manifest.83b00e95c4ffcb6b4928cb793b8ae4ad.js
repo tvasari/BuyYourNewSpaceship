505638e9d69f00d6f814354ac994dec24e6145ef
Tommaso <tommaso.vsr@gmail.com>
2020-04-04 18:48:33 +0200

    Updates

---
+++ precache-manifest.83b00e95c4ffcb6b4928cb793b8ae4ad.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9d06d64365c08b6f5f6e706166697b6",
+    "url": "/BuyYourNewSpaceship/index.html"
+  },
+  {
+    "revision": "bc2031c27084357c6ad6",
+    "url": "/BuyYourNewSpaceship/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "192fe61aa01e0bf6cfc7",
+    "url": "/BuyYourNewSpaceship/static/css/main.3eea6116.chunk.css"
+  },
+  {
+    "revision": "bc2031c27084357c6ad6",
+    "url": "/BuyYourNewSpaceship/static/js/2.d48e123f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/BuyYourNewSpaceship/static/js/2.d48e123f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "192fe61aa01e0bf6cfc7",
+    "url": "/BuyYourNewSpaceship/static/js/main.4923916c.chunk.js"
+  },
+  {
+    "revision": "bc8888f1e4a5f545e727",
+    "url": "/BuyYourNewSpaceship/static/js/runtime-main.7fc3fd85.js"
+  },
+  {
+    "revision": "7887b01603439fe6c30e366f6fa0857f",
+    "url": "/BuyYourNewSpaceship/static/media/deathstar.7887b016.png"
+  },
+  {
+    "revision": "0b5dd05f608625ba3820f8e8c19f328c",
+    "url": "/BuyYourNewSpaceship/static/media/deep-space.0b5dd05f.jpg"
+  },
+  {
+    "revision": "f95660bfdb42f5237dee0629775e4112",
+    "url": "/BuyYourNewSpaceship/static/media/ef76nebulon-bescortfrigate.f95660bf.png"
+  },
+  {
+    "revision": "c9833e4a1b98081a8d4d099bc723697e",
+    "url": "/BuyYourNewSpaceship/static/media/executor.c9833e4a.png"
+  },
+  {
+    "revision": "40c3b0b7124fb907e2651a45bbbd3257",
+    "url": "/BuyYourNewSpaceship/static/media/imperialshuttle.40c3b0b7.png"
+  },
+  {
+    "revision": "24b5ba990f1f996b1ed15cd631a63961",
+    "url": "/BuyYourNewSpaceship/static/media/millenniumfalcon.24b5ba99.png"
+  },
+  {
+    "revision": "32921d4847bc7a128f3d0ea76aa474e1",
+    "url": "/BuyYourNewSpaceship/static/media/sentinel-classlandingcraft.32921d48.png"
+  },
+  {
+    "revision": "c45359a8a87041f4b36f47d0b7283c9f",
+    "url": "/BuyYourNewSpaceship/static/media/slave1.c45359a8.png"
+  },
+  {
+    "revision": "8149544a3ca473ef497187c8e9700cad",
+    "url": "/BuyYourNewSpaceship/static/media/tieadvancedx1.8149544a.png"
+  },
+  {
+    "revision": "ca146ec24c0f2dc61557d9dc3244183f",
+    "url": "/BuyYourNewSpaceship/static/media/x-wing.ca146ec2.png"
+  },
+  {
+    "revision": "adb26b079dee81bfa7c3016fe775441c",
+    "url": "/BuyYourNewSpaceship/static/media/y-wing.adb26b07.png"
+  }
+]);
